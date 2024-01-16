b5e966961f293afbcf17e516661c08c9a8d2a783
Holidaez <holidaez@DESKTOP-GG36629>
2024-01-16 16:11:53 -0500

    class based components

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,44 @@ import PokemonShowcase from './components/PokemonShowcase'
 import './App.css'
 import StatsComponent from './components/statsComponent'
 import PokeMoves from './components/PokeMoves'
+import { Component } from 'react'
 
 
-function App() {
+// function App() {
+
+//   return (
+//     <div className='background'>
+//     <PokemonShowcase/>
+//     <StatsComponent/>
+//     {/* <PokeMoves/> */}
+//     <PokeMoves>Something</PokeMoves>
+//     </div>
+//   )
+// }
+
+// export default App
 
 
-  return (
-    <div className='background'>
-    <PokemonShowcase/>
-    <StatsComponent/>
-    {/* <PokeMoves/> */}
-    <PokeMoves>Something</PokeMoves>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: {
+        firstName: 'Keegan',
+        lastName: 'Abley'
+      }
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        Hello {this.state.name.firstName} {this.state.name.lastName}
+        <PokemonShowcase stats={{def:50}}/>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/components/PokemonShowcase.jsx
+++ src/components/PokemonShowcase.jsx
@@ -1,29 +1,68 @@
 // import squirtle from '../assets/squirtle.png'
-import { useContext } from 'react'
+import { Component, useContext } from 'react'
 import './PokemonShowcase.css'
 import { StatsContext } from '../context/StatsContext'
 
 
-function PokemonShowcase () {
-    console.log("Pokemon Showcase Rendered")
+// function PokemonShowcase () {
+//     console.log("Pokemon Showcase Rendered")
 
-    const {stats} = useContext(StatsContext)
-
-
-
-    const favPokemon = "Squirtle"
-    const pokeCharacteristics = {level:'5', type:"Water", highestStat:stats.def}
+//     const {stats} = useContext(StatsContext)
 
 
 
-    return(
-        <div>
-            <h1 style={{color:'blue'}}>{favPokemon}&apos;s Pokemon Showcase Component</h1>
-            <h3>Pokemon Characteristics</h3>
-            <div className='poke-characteristics-div'>Level: {pokeCharacteristics.level} Type: {pokeCharacteristics.type} Highest Stat: {pokeCharacteristics.highestStat} </div>
-            <img src='./squirtle.png' alt="Image of the pokemon Squirtle"/>
-        </div>
-    )
+//     const favPokemon = "Squirtle"
+//     const pokeCharacteristics = {level:'5', type:"Water", highestStat:stats.def}
+
+
+
+//     return(
+//         <div>
+//             <h1 style={{color:'blue'}}>{favPokemon}&apos;s Pokemon Showcase Component</h1>
+//             <h3>Pokemon Characteristics</h3>
+//             <div className='poke-characteristics-div'>Level: {pokeCharacteristics.level} Type: {pokeCharacteristics.type} Highest Stat: {pokeCharacteristics.highestStat} </div>
+//             <img src='./squirtle.png' alt="Image of the pokemon Squirtle"/>
+//         </div>
+//     )
+// }
+
+// export default PokemonShowcase
+
+class PokemonShowcase extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            test: false
+        }
+    }
+
+    componentDidMount() {
+        console.log("Inside on mount", this.state.test)
+        this.setState(() => this.state.test = true)
+    }
+
+    componentDidUpdate() {
+        console.log("Component updated fired", this.state.test)
+        if(this.state.test === true) {
+            console.log("the state is truthy")
+        }
+    }
+
+    componentWillUnmount() {
+        console.log("Component Unmounted")
+    }
+
+    render() {
+        return (
+            <div>
+                {console.log(this.props)}
+                <div>
+                    {this.props.stats.def}
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default PokemonShowcase
